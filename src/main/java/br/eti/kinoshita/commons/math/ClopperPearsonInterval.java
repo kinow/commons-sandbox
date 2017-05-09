7c93724718cec1491cb7fed519480b646a9cffab
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-05-09 21:44:42 +1200

    Add tests for Clopper Pearson interval

---
+++ src/main/java/br/eti/kinoshita/commons/math/ClopperPearsonInterval.java
@@ -0,0 +1,14 @@
+package br.eti.kinoshita.commons.math;
+
+import org.apache.commons.math4.stat.interval.IntervalUtils;
+import org.hipparchus.stat.interval.BinomialProportion;
+
+public class ClopperPearsonInterval {
+
+    public static void main(String[] args) {
+        // both throw an exception
+        //BinomialProportion.getClopperPearsonInterval(1, 0, 0.095);
+        // IntervalUtils.getClopperPearsonInterval(1, 0, 0.95);
+    }
+
+}
