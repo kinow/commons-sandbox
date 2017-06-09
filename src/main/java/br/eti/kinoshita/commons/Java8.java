3dbc55696766d70b3402f21045487125df7f394c
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-06-09 18:02:55 +1200

    To remember

---
+++ src/main/java/br/eti/kinoshita/commons/Java8.java
@@ -0,0 +1,12 @@
+package br.eti.kinoshita.commons;
+
+import java.util.stream.IntStream;
+
+public class Java8 {
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 8).forEach((i) -> {
+            System.out.println(String.format("hello %d", i));
+        });
+    }
+}
